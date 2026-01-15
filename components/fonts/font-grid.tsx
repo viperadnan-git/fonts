ca401303e727debbb79f551f3d49e9970947ce39
Adnan Ahmad <viperadnan@gmail.com>
2026-01-15 16:21:21 +0530

    refactor: change homepage to single column layout

--- components/fonts/font-grid.tsx
+++ components/fonts/font-grid.tsx
@@ -15,7 +15,7 @@ export function FontGrid({ families }: FontGridProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+    <div className="grid grid-cols-1 gap-6">
       {families.map((family) => (
         <FontCard key={family.id} family={family} />
       ))}
